9597a05f337e7bae876b82539a1200590e8fa1a6
Peter Walsh <xorgate@gmail.com>
2023-09-24 19:49:41 -0230

    Adding index that reexports classes

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { Point3, Vector2, Vector3, Vector4 } from "./Vector"
+import { Quaternion } from "./Quaternion"
+import { Matrix2, Matrix3, Matrix4, Matrix4Transform } from "./Matrix"
+
+export default {
+    Point3,
+    Vector2,
+    Vector3,
+    Vector4,
+    Quaternion,
+    Matrix2,
+    Matrix3,
+    Matrix4,
+    Matrix4Transform
+}
